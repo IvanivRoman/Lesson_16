bb386265d9f0ae89d11e1900b756ca450df3254d
Roman <ivaniv.roma.15@gmail.com>
2022-12-07 00:05:52 +0200

    create three methods with streams in App class

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private int age;
+    private Sex sex;
+
+    public Person() {}
+
+    public Person(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (!name.equals(person.name)) return false;
+        return sex == person.sex;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + sex.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,112 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.assertj.core.api.Assertions.*;
+
+class AppTest {
+    private static List<Person> list, list1, list2, list3, adultWomen, adultWomen1;
+
+    @BeforeAll
+    static void initializeLists() {
+        list = List.of(new Person("Teodor", 16, Sex.MAN),
+                new Person("Piter", 23, Sex.MAN),
+                new Person("Olena", 42, Sex.WOMAN),
+                new Person("Ivan Ivanovich", 69, Sex.MAN),
+                new Person("Vasuluna", 17, Sex.WOMAN),
+                new Person("Christine", 25, Sex.WOMAN),
+                new Person("Nazar", 16, Sex.MAN),
+                new Person("Oleg", 37, Sex.MAN),
+                new Person("Gordon", 27, Sex.MAN));
+
+        list1 = List.of(new Person("Teodor", 16, Sex.MAN),
+                new Person("Piter", 23, Sex.MAN),
+                new Person("Ivan Ivanovich", 69, Sex.MAN),
+                new Person("Nazar", 16, Sex.MAN),
+                new Person("Oleg", 37, Sex.MAN),
+                new Person("Gordon", 27, Sex.MAN));
+
+        list2 = new ArrayList<>();
+
+        list3 = null;
+
+        adultWomen = List.of(
+                new Person("Olena", 42, Sex.WOMAN),
+                new Person("Christine", 25, Sex.WOMAN));
+
+        adultWomen1 = List.of(
+                new Person("Olena", 42, Sex.WOMAN),
+                new Person("Vasuluna", 17, Sex.WOMAN),
+                new Person("Christine", 25, Sex.WOMAN));
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void testSelectAdultWomenWithSameList() {
+        assertThat(App.selectAdultWomen(list)).isEqualTo(adultWomen);
+        assertThat(App.selectAdultWomen(list)).containsAll(adultWomen);
+    }
+
+    @Test
+    void testSelectAdultWomenWithNotSameList() {
+        assertThat(App.selectAdultWomen(list)).isNotEqualTo(adultWomen1);
+        assertThat(App.selectAdultWomen(list)).isNotEqualTo(list1);
+    }
+
+    @Test
+    void shouldSelectAdultWomenReturnEmptyList() {
+        assertThat(App.selectAdultWomen(list2)).isEmpty();
+    }
+
+    @Test
+    void shouldSelectAdultWomenThrowExceptionOnNullList() {
+        Throwable thrown = catchThrowable(() -> App.selectAdultWomen(list3));
+        assertThat(thrown).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void shouldAverageMenAgeReturnTrueNumber() {
+        assertThat(App.averageMenAge(list)).isEqualTo(31.0);
+        assertThat(App.averageMenAge(list1)).isEqualTo(31.0);
+    }
+
+    @Test
+    void testAverageMenAgeWithNoMenInList() {
+        assertThat(App.averageMenAge(adultWomen)).isEqualTo(-1);
+        assertThat(App.averageMenAge(adultWomen1)).isEqualTo(-1);
+    }
+
+    @Test
+    void shouldAverageMenAgeReturnNegativeOnEmptyList() {
+        assertThat(App.averageMenAge(list2)).isEqualTo(-1);
+    }
+
+    @Test
+    void shouldAverageMenAgeThrowExceptionOnNullList() {
+        Throwable thrown = catchThrowable(() -> App.averageMenAge(list3));
+        assertThat(thrown).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void shouldCountAdultsReturnTrueNumber() {
+        assertThat(App.countAdults(list)).isEqualTo(5);
+        assertThat(App.countAdults(list1)).isEqualTo(3);
+        assertThat(App.countAdults(adultWomen)).isEqualTo(2);
+        assertThat(App.countAdults(adultWomen1)).isEqualTo(2);
+        System.out.println(App.countAdults(list2));
+    }
+
+    @Test
+    void shouldCountAdultsReturnZeroOnEmptyList() {
+        assertThat(App.countAdults(list2)).isZero();
+    }
+
+    @Test
+    void shouldCountAdultsThrowExceptionOnNullList() {
+        Throwable thrown = catchThrowable(() -> App.countAdults(list3));
+        assertThat(thrown).isInstanceOf(NullPointerException.class);
     }
 }
